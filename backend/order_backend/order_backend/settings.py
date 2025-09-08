dcb349617b3930ac92b57d7004cee2770a169e07
Navithkumar <navithkumarkratos@gmail.com>
2025-09-08 13:41:29 +0530

    utlits | part _1

--- backend/order_backend/order_backend/settings.py
+++ backend/order_backend/order_backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'order_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': 'root',
+        'PASSWORD': os.getenv("MYSQL_ROOT_PASSWORD"),
+        'HOST': os.getenv("MYSQL_HOST"),
+        'PORT': '3306',
     }
 }
 
