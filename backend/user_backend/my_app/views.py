34dea18dd8fb2f353f5f7a7c5cbe2f5676ca0d8c
Navithkumar <navithkumarkratos@gmail.com>
2025-09-09 14:06:12 +0530

    docker | part_2| update

--- backend/user_backend/my_app/views.py
+++ backend/user_backend/my_app/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 
 def hello(request):
-    return JsonResponse({"message": "Hello navith test"})
+    return JsonResponse({"message": "Hello navith "})
 
